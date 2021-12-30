02c367e6d51e1e6382827a9bf5dec24c7de5b026
Thoybur Rohman <73062879+Thoybur-Rohman@users.noreply.github.com>
2021-12-30 13:58:09 +0000

    Update TwoSum.java

--- Leet Code/1- TwoSum/src/TwoSum.java
+++ Leet Code/1- TwoSum/src/TwoSum.java
@@ -51,9 +51,9 @@ public class TwoSum {
             public static int[] twoSum3(int[] nums, int target) {
                 Map<Integer, Integer> map = new HashMap<>();
                 for (int i = 0; i < nums.length; i++) {
-                    int complement = target - nums[i];
-                    if (map.containsKey(complement)) {
-                        return new int[] { map.get(complement), i };
+                    int sum = target - nums[i];
+                    if (map.containsKey(sum)) {
+                        return new int[] { map.get(sum), i };
                     }
                     map.put(nums[i], i);
                 }
