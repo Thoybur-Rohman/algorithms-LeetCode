990089558f5258c442e871ae337aa5a121d8ec75
Thoybur Rohman <73062879+Thoybur-Rohman@users.noreply.github.com>
2022-01-14 11:31:24 +0000

    updated answer

--- Leet Code/isPalindrome/src/isPalindrome.java
+++ Leet Code/isPalindrome/src/isPalindrome.java
@@ -10,10 +10,13 @@ public class isPalindrome {
             String str = String.valueOf(x);
             int start = 0;
             int end = str.length() -1;
+           
             while(start < end) {
                 if (str.charAt(start) != str.charAt(end)) {
                     return false;
                 }
+                System.out.print(str.charAt(start));  
+                System.out.print(str.charAt(end));  
                 start++;
                 end--;
             }
