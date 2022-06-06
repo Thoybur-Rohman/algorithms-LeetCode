d55db5e75a270a96fcc2e10f4324cccbec203779
Thoybur Rohman <2026156@brunel.ac.uk>
2022-06-06 16:04:12 +0100

    Update PlusOne.java

--- Leet Code/PlusOne/PlusOne.java
+++ Leet Code/PlusOne/PlusOne.java
@@ -1,7 +1,7 @@
 class Solution{
     public int[]plusOne(int[]digits){
        intndigits.length;
-        for(inti-n-1;i>-0;i--){
+        for(int i= n-1;i>-0;i--){
             if(digits[i]<9){
                digits[i]++;
                 return digits;
