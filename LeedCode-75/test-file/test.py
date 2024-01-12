115ef9c4f847529c4f4de1aa5940cc0333eaa3d9
Thoy <2026156@brunel.ac.uk>
2024-01-12 18:13:54 +0000

    Create test.py

---
+++ LeedCode-75/test-file/test.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        n = len(candies)
+        maximum = max(candies)
+        answerList = []
+        for i in range(n):
+            sumValue = candies[i] + extraCandies
+            if sumValue >= maximum:
+                answerList.append(True)
+            else:
+                answerList.append(False)
+
+        return answerList
